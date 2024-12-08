238e5d7b8ee436408c275f2e77abc5b290c15374
gzhegow <6562680@gmail.com>
2024-12-08 22:14:49 +0300

    main

--- src/PipelineFactoryInterface.php
+++ src/PipelineFactoryInterface.php
@@ -3,6 +3,53 @@
 namespace Gzhegow\Pipeline;
 
 
+use Gzhegow\Pipeline\Process\PipelineProcess;
+use Gzhegow\Pipeline\Process\MiddlewareProcess;
+use Gzhegow\Pipeline\Process\PipelineProcessInterface;
+use Gzhegow\Pipeline\Chain\PipelineChain as PipelineChain;
+use Gzhegow\Pipeline\Chain\MiddlewareChain as MiddlewareChain;
+
+
 interface PipelineFactoryInterface
 {
+    public function newFacade(
+        PipelineProcessManagerInterface $processManager
+    ) : PipelineFacadeInterface;
+
+
+    public function newProcessor() : PipelineProcessorInterface;
+
+    public function newProcessManager(
+        PipelineProcessorInterface $processor
+    ) : PipelineProcessManagerInterface;
+
+
+    public function newPipeline() : PipelineChain;
+
+    public function newMiddleware($from) : MiddlewareChain;
+
+
+    public function newMiddlewareProcess(
+        PipelineProcessManagerInterface $processManager,
+        //
+        MiddlewareChain $middleware
+    ) : ?MiddlewareProcess;
+
+    public function newPipelineProcess(
+        PipelineProcessManagerInterface $processManager,
+        //
+        PipelineChain $pipeline
+    ) : ?PipelineProcess;
+
+
+    public function newProcessFrom(PipelineProcessManagerInterface $processManager, $from) : ?PipelineProcessInterface;
+
+    public function newProcessFromInstance($from) : ?PipelineProcessInterface;
+
+    public function newProcessFromMiddleware(PipelineProcessManagerInterface $processManager, $from) : ?MiddlewareProcess;
+
+    public function newProcessFromPipeline(PipelineProcessManagerInterface $processManager, $pipeline) : ?PipelineProcess;
+
+
+    public function newHandlerObject(string $class, array $parameters = []) : object;
 }
